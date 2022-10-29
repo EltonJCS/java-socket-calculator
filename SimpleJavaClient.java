a3ec47dcb19f6e8084c7e6ecb6db2c86d32eacb0
Elton Jatobá <80626909+EltonJCS@users.noreply.github.com>
2022-10-28 22:53:11 -0300

    Update SimpleJavaClient.java

--- SimpleJavaClient.java
+++ SimpleJavaClient.java
@@ -2,10 +2,12 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.Socket;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class SimpleJavaClient {
 
     public static void main(String[] args) 	{
+
         try {
             Socket socket = new Socket("127.0.0.1", 9999);
             Scanner scanner = new Scanner(System.in);
@@ -16,9 +18,30 @@ public class SimpleJavaClient {
             while (true) {
                 System.out.print("Insira a equação (operando operador operando): ");
                 String input = scanner.nextLine();
+
+                //Separa a entrada do cliente em tokens
+                StringTokenizer tokenizer = new StringTokenizer(input);
+
+                //Verifica se a equação possui 3 tokens (operando, operador e operando)
+                if (tokenizer.countTokens() != 3){
+                    System.out.println("Somente operações entre 2 operandos!");
+                    continue;
+                }
+
+                //Verifica se a equação segue o formato esperado (operando, operador e operando)
+                try{
+                    Float.valueOf(tokenizer.nextToken());
+                    tokenizer.nextToken();
+                    Float.valueOf(tokenizer.nextToken());
+                } catch (NumberFormatException e){
+                    System.out.println("Operando não é um número!");
+                    continue;
+                }
+
                 //Envia a equação para o servidor
                 dos.writeUTF(input);
-                //Armazena a resposa do servidor e mostra ao cliente
+
+                //Armazena a resposta do servidor e mostra ao cliente
                 String ans = dis.readUTF();
                 System.out.println("Answer=" + ans);
             }
